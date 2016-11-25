4e2055876876ddcdaaa5c4fa018706213855b5b8
vishalivishwanathan <vishali.jagan1997@gmail.com>
2016-11-25 21:22:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/', 'index.html'));
+  res.sendFile(path.join(__dirname  , req.url));
 });
 
 app.get('/ui/style.css', function (req, res) {
